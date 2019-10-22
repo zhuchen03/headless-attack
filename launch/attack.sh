f5d1596f7142c371bfb02d67adaec38108e7b1ce
Avi Schwarzschild <avi1@math.umd.edu>
2019-10-21 22:53:51 -0400

    pgd added to same bash.

--- launch/attack.sh
+++ launch/attack.sh
@@ -12,9 +12,10 @@ pgd_steps=${10}
 pgd_lr=${11}
 eps=${12}
 
-    nohup \
-    python attack.py --subs-chk-name ${subs_chk} --subs-net ${subs_net} --target-net ${target_net} --target-chk-name ${target_chk} --centroid-out-name ${cname}  --seed ${seed} --gpu ${gpu} --overwrite --n-variants ${nvars} --gauss-std ${gaussstd} --pgd-steps ${pgd_steps} --pgd-lr ${pgd_lr} --eps ${eps} \
-    > logs/subs${subs_net}-${subs_chk}-tgt${target_net}-${target_chk}-seed${seed}-nvars${nvars}-gstd${gaussstd}-pgdsteps${pgd_steps}-pgdlr${pgd_lr}-4800samples.log 2>&1 &
+    #nohup \
+    python attack.py --subs-chk-name ${subs_chk} --subs-net ${subs_net} --target-net ${target_net} --target-chk-name ${target_chk} --centroid-out-name ${cname}  --seed ${seed} --gpu ${gpu} --overwrite --n-variants ${nvars} --gauss-std ${gaussstd} --pgd-steps ${pgd_steps} --pgd-lr ${pgd_lr} --eps ${eps}
+    python attack.py --pgd --subs-chk-name ${subs_chk} --subs-net ${subs_net} --target-net ${target_net} --target-chk-name ${target_chk} --centroid-out-name ${cname}  --seed ${seed} --gpu ${gpu} --overwrite --n-variants ${nvars} --gauss-std ${gaussstd} --pgd-steps ${pgd_steps} --pgd-lr ${pgd_lr} --eps ${eps}
+    #> logs/subs${subs_net}-${subs_chk}-tgt${target_net}-${target_chk}-seed${seed}-nvars${nvars}-gstd${gaussstd}-pgdsteps${pgd_steps}-pgdlr${pgd_lr}-4800samples.log 2>&1 &
 
 }
 
